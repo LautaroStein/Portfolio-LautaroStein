aa13a81aecf0dac6ff9d63394862f4812dd9f172
LautaroStein <steinlautaroweb@gmail.com>
2022-01-25 09:07:48 -0300

    menu terminado

---
+++ pages/components/Navbar.js
@@ -0,0 +1,45 @@
+import Link from "next/link"
+import Image from "next/image"
+import { useState } from "react"
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs"
+import { SiGmail } from "react-icons/si"
+
+
+export const Navbar = () => {
+
+    const [open, setOpen] = useState(false)
+
+    return(
+        <>
+        <nav className="contenedor-navegacion">
+            <div className="contenedor-boton-nav">
+                <Image src="/Logo.svg" alt="Picture of the author" width={100} height={100} />
+                <div>
+                    <button onClick={() => setOpen(!open)}>
+                        <span className={`${open === true && "nav-open"}`}>CERRAR</span>
+                        <span className={`${open === false && "nav-close"}`}>MENU</span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <p>CONTACTAME</p>
+            </div>
+        </nav>
+        <div className={`contenedor-navegacion-dos ${open === true && "nav-open-dos"}`}>
+        </div>
+        <div className={`contenedor-navegacion-tres ${open === true && "nav-open-tres"}`}>
+            <ul className={`social-links ${open === true && "animation-social"}`}>
+                <li><Link href="/"><BsGithub /></Link></li>
+                <li><Link href="/"><BsInstagram /></Link></li>
+                <li><Link href="/"><BsLinkedin /></Link></li>
+                <li><Link href="/"><SiGmail /></Link></li>
+            </ul>
+            <div className={`navegacion-links ${open === true && "animation-write"}`}>
+                <Link href="/"><a>Inicio</a></Link>
+                <Link href="/"><a>Sobre mí</a></Link>
+                <Link href="/"><a>Trabajos</a></Link>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const contact = () => {
+    return (
+        <>
+            <h1>Page contact</h1>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </>
+    )
+}
+
+export default contact
